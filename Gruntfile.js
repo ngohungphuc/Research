d88fd06e868ba45a7f9b3f2ae331b878f09906bb
hoangphucvu <ngohungphuc95@gmail.com>
2016-08-30 09:24:19 +0700

    Code example 11-task-aliasing

--- Gruntfile.js
+++ Gruntfile.js
@@ -131,4 +131,17 @@ module.exports = function(grunt) {
     grunt.registerTask('task-object', function() {
         console.log('The "%foo" has arguments %j', this.name, this.args);
     });
+
+    //Code example 11-task-aliasing
+    grunt.registerTask('build', function() {
+        console.log('building...');
+    });
+    grunt.registerTask('test', function() {
+        console.log('testing...');
+    });
+    grunt.registerTask('upload', function() {
+        console.log('uploading...');
+    });
+
+    grunt.registerTask('deploy', ['build', 'test', 'upload']);
 };
