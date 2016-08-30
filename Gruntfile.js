c4c34c4298ea6135c77af3ab3e08a4c7d828d92c
hoangphucvu <ngohungphuc95@gmail.com>
2016-08-30 10:18:17 +0700

    Code example 14-task-help

--- Gruntfile.js
+++ Gruntfile.js
@@ -189,4 +189,21 @@ module.exports = function(grunt) {
             }
         });
     });
+
+    //Code example 14-task-help
+    grunt.registerTask('analyze',
+        'Analyzes the source',
+        function() {
+            console.log('analyzing...');
+        }
+    );
+    grunt.registerMultiTask('compile',
+        'Compiles the source',
+        function() {
+            console.log('compiling...');
+        }
+    );
+    grunt.registerTask('all',
+        'Analyzes and compiles the source', ['analyze', 'compile']
+    );
 };
