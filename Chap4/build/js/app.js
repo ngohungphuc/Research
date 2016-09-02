9ceac0af4bfe673cc3a5afe2ba91d43a9fda1b5e
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-09-02 21:59:24 +0700

    View and css

--- Chap4/build/js/app.js
+++ Chap4/build/js/app.js
@@ -1,14 +1,4 @@
 (function() {
-  var add, subtract;
-
-  add = function(a, b) {
-    return a + b;
-  };
-
-  subtract = function(a, b) {
-    return a – b;
-  };
-
   alert(add(7, subtract(4, 1)));
 
 }).call(this);
--- chap4/Gruntfile.js
+++ chap4/Gruntfile.js
@@ -1,31 +1,35 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-stylus');
-	grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-jade');
 
-	grunt.initConfig({
-		coffee: {
-			build: {
-				src: 'src/scripts/app.coffee',
-				dest: 'build/js/app.js'
-			}
-		},
-		stylus: {
-			build: {
-				src: 'src/styles/app.styl',
-				dest: 'build/css/app.css'
-			}
-		},
-		jade: {
-			build: {
-				options: {
-					pretty: true
-				},
-				src: 'src/views/app.jade',
-				dest: 'build/app.html'
-			}
-		}
-	});
-	grunt.log.write('Grunt is running\n');
-	grunt.registerTask('default' , ['coffee', 'stylus', 'jade']);
+    grunt.initConfig({
+        coffee: {
+            build: {
+                src: 'src/scripts/app.coffee',
+                dest: 'build/js/app.js'
+            }
+        },
+        stylus: {
+            options: {
+                compress: false,
+                linenos:true
+            },
+            build: {
+                src: 'src/styles/app.styl',
+                dest: 'build/css/app.css'
+            }
+        },
+        jade: {
+            build: {
+                options: {
+                    pretty: true
+                },
+                src: 'src/views/app.jade',
+                dest: 'build/app.html'
+            }
+        }
+    });
+    grunt.log.write('Grunt is running\n');
+    grunt.registerTask('default', ['coffee', 'stylus', 'jade']);
 };
