94ccdeac46ba93f0e62d7671a7013947f494b96b
hoangphucvu <ngohungphuc95@gmail.com>
2016-12-09 12:00:02 +0700

    Using preloader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
     },
     //watch:true
     module:{
+        preLoaders:[
+            {
+                test: /\.js%/,
+                exclude:/node_modules/,
+                loader:'jshint-loader'
+            }
+        ],
         loaders:[
             {
                 test: /\.es6$/,
