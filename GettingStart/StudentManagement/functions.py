065df60f5049d603fb98d69253d046c0c33453d3
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-07-09 19:36:51 +0700

    Opening, Reading, and Writing

--- GettingStart/StudentManagement/functions.py
+++ GettingStart/StudentManagement/functions.py
@@ -5,7 +5,7 @@ def get_students_titlecase():
     students_titlecase = []
     for student in students:
         # returns a copy of the string in which first characters of all the words are capitalized
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 
@@ -19,9 +19,31 @@ def add_student(name, student_id=322):
     students.append(student)
 
 
-student_list = get_students_titlecase()
+def save_file(student):
+    try:
+        # a to append
+        f = open('student.txt', 'a')
+        f.write(student + '\n')
+        f.close()
+    except Exception:
+        print('Could not save file')
+
+
+def read_file():
+    try:
+        f = open('student.txt', 'r')
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print('Could not read file')
+
+
+read_file()
+print_students_titlecase()
+
 student_name = input("Enter student name: ")
 student_id = input("Enter student id: ")
 
 add_student(student_name, student_id)
-print_students_titlecase()
+save_file(student_name)
