eb15925babb44506a8808c5a26aced3a78c5f39e
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-07-12 23:10:44 +0700

    inheritance & polymorphism

--- GettingStart/StudentManagement/classess.py
+++ GettingStart/StudentManagement/classess.py
@@ -2,16 +2,32 @@ students = []
 
 
 class Student:
+    school_name = "TDT"
+
     # pass keyword tell interpreter do nothing
     # self keyword refer to instance of the class
     def __init__(self, name, student_id=322):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
+
+    def get_name_capitialize(self):
+        return self.name.capitialize()
+
+    def get_school_name(self):
+        return self.school_name
 
 
-mark = Student("Mark")
+# inherit from Student class
+class HighSchollStudent(Student):
+    school_name = "Nash tech"
 
-print(mark)
+    def get_school_name(self):
+        return "this is high scholl student"
+
+    def get_name_capitialize(self):
+        original_value = super().get_name_capitialize()
+        return original_value + " -hs"
