8919461ca77cb9ff893bf76eed9a12e7e340579a
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-11 13:47:46 +0700

    update repo

--- Python-Beyond the basic/module1/reader/__main__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import reader
-
-r = reader.Reader(sys.argv[1])
-try:
-    print(r.reader)
-finally:
-    r.close()
--- Python-Beyond the basic/module1/reader/reader/compressed/gzipped.py
+++
@@ -1,9 +0,0 @@
-import gzip
-import sys
-
-opener = gzip.open
-
-if __name__ == 'main':
-    f = gzip.open(sys.argv[1], mode='wt')
-    f.write(' '.join(sys.argv[2:]))
-    f.close()
