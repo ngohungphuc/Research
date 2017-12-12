b248081d94852e4e0ce34eb4d04c2316e71e7921
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-12 17:52:22 +0700

    add skeleton project

---
+++ course-project/public/src/js/feed.js
@@ -0,0 +1,15 @@
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
