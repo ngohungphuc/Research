05922ca238cea987fff2d21644618f79995d4c46
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-31 22:46:23 +0700

    cache multiple file with addAll

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -4,7 +4,21 @@ self.addEventListener("install", function(event) {
     caches.open("static").then(function(cache) {
       console.log("Service Worker] Precaching app shell");
       //add new resource
-      cache.add("/src/js/app.js");
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
     })
   );
 });
