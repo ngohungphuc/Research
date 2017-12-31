d9c5223cc7260a1726facbf9bd244f88431464a1
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-31 23:03:09 +0700

    dynamic caching

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -5,19 +5,19 @@ self.addEventListener("install", function(event) {
       console.log("Service Worker] Precaching app shell");
       //add new resource
       cache.addAll([
-        '/',
-        '/index.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/promise.js',
-        '/src/js/fetch.js',
-        '/src/js/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
       ]);
     })
   );
@@ -34,7 +34,12 @@ self.addEventListener("fetch", function(event) {
       if (response) {
         return response;
       } else {
-        return fetch(event.request);
+        return fetch(event.request).then(function(res) {
+          return caches.open("dynamic").then(function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
       }
     })
   );
