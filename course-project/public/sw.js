fc873cb03a0aa4acda4ae33764f7371a9148720e
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-31 22:21:26 +0700

    static caching precaching

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,6 +1,14 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+    .then(function(cache) {
+      console.log('Service Worker] Precaching app shell');
+      //add new resource
+      cache.add('/src/js/app.js')
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
