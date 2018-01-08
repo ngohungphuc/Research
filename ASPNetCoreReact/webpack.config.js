9626751929d5aa5cbf2a0e33356f011ac983449a
ngohungphuc <ngohungphuc95@gmail.com>
2018-01-08 15:19:31 +0700

    Extract-text-webpack-plugin and uglifyjs-webpack-plugin

--- ASPNetCoreReact/webpack.config.js
+++ ASPNetCoreReact/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('allstyles.css');
 
 module.exports = {
   entry: "./wwwroot/source/app.js",
@@ -8,19 +10,21 @@ module.exports = {
     filename: "bundle.js"
   },
   plugins: [
+    extractCSS,
     //explicitly importing jquery in our files
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery",
       Popper: ["popper.js", "default"]
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        use: extractCSS.extract(['css-loader?minimize'])
       },
       {
         test: /\.js?$/,
