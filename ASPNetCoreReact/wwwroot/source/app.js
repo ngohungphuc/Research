186c676bec863aa85a8cc1271a27e5ae3b8b378a
ngohungphuc <ngohungphuc95@gmail.com>
2018-01-08 15:31:45 +0700

    add react

--- ASPNetCoreReact/wwwroot/source/app.js
+++ ASPNetCoreReact/wwwroot/source/app.js
@@ -1,12 +1,15 @@
-﻿require("./lib");
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/site.css';
-import ES6Lib from './es6codelib';
-document.getElementById("fillthis").innerHTML = getText();
-$("#fillthiswithjquery").html("Filled by Jquery! ??");
-
-let myES6Object = new ES6Lib();
-$('#fillthiswithes6lib').html(myES6Object.getData());
-
+﻿//$ = require('jquery');
+require("./lib");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/site.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./reactcomponent";
+import ES6Lib from "./es6codelib";
+ReactDOM.render(<Counter />, document.getElementById("basicreactcomponent"));
+//document.getElementById("fillthis").innerHTML = getText();
+//$('#fillthiswithjquery').html('Filled by Jquery??');
+//let myES6Object = new ES6Lib();
+//$('#fillthiswithes6lib').html(myES6Object.getData());
 //We also need to let HMR know that we are ready to “accept” Hotmodule updates from within our module.
 module.hot.accept();
