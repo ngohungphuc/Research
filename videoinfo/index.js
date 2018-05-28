db32becd1b91f9afe140918e8ef84832d2bb68d1
Tony Hudson <ngohungphuc95@gmail.com>
2018-05-29 00:25:48 +0700

    load index file

---
+++ videoinfo/index.js
@@ -0,0 +1,11 @@
+const electron = require('electron');
+
+const {
+    app,
+    BrowserWindow
+} = electron;
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
