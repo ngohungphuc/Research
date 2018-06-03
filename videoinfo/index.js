7600744ea1ce00753df74edd8abf6e30c229a7c3
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-03 22:51:20 +0700

    update code

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,10 +1,19 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const {app, BrowserWindow, ipcMain} = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', () => {});
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        mainWindow
+            .webContents
+            .send('video:metadata', metadata.format.duration);
+    });
+});
