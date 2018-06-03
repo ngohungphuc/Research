2c8a9ec8831e37bc31c32c7e467f7d027b36534e
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-03 22:00:04 +0700

    ipc communication

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,11 +1,10 @@
 const electron = require('electron');
 
-const {
-    app,
-    BrowserWindow
-} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
+
+ipcMain.on('video:submit', () => {});
