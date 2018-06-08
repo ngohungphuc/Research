94d4b9b8ccb0ae31802d09aa600be90fe84c0e89
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-09 00:02:05 +0700

    construct menu bar

---
+++ todo/index.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+    {
+        label : 'File'
+    }
+];
