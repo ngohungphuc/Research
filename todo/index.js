e03df9269de25fa97eb2329bbb2e409ec6e383db
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-09 21:08:58 +0700

    creating seperate window

--- todo/index.js
+++ todo/index.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
@@ -11,12 +12,19 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+    addWindow = new BrowserWindow({width: 300, height: 200, title: 'Add new Todo'})
+}
+
 const menuTemplate = [
     {
         label: 'File',
         submenu: [
             {
-                label: 'New Todo'
+                label: 'New Todo',
+                click() {
+                    createAddWindow();
+                }
             }, {
                 label: 'Quit',
                 //short key
