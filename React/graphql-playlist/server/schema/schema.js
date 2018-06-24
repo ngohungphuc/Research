705da1d5d71242ce4d9d43b7a6dbdd683c134160
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-24 10:20:29 +0700

    query relationship for author type

--- React/graphql-playlist/server/schema/schema.js
+++ React/graphql-playlist/server/schema/schema.js
@@ -5,26 +5,45 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 var books = [{
         name: 'Name of the Wind',
         genre: 'Fantasy',
         id: '1',
-        authorId: "1"
+        authorId: '1'
     },
     {
         name: 'The Final Empire',
         genre: 'Fantasy',
         id: '2',
-        authorId: "2"
+        authorId: '2'
+    },
+    {
+        name: 'The Hero of Ages',
+        genre: 'Fantasy',
+        id: '4',
+        authorId: '2'
     },
     {
         name: 'The Long Earth',
         genre: 'Sci-Fi',
         id: '3',
-        authorId: "3"
+        authorId: '3'
+    },
+    {
+        name: 'The Colour of Magic',
+        genre: 'Fantasy',
+        id: '5',
+        authorId: '3'
+    },
+    {
+        name: 'The Light Fantastic',
+        genre: 'Fantasy',
+        id: '6',
+        authorId: '3'
     },
 ];
 
@@ -80,6 +99,14 @@ const AuthorType = new GraphQLObjectType({
         },
         age: {
             type: GraphQLInt
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return _.filter(books, {
+                    authorId: parent.id
+                })
+            }
         }
     })
 })
