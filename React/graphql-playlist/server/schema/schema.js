515bb5f52dc840fc3958c0d378ae9693f7a2dd4b
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-29 13:13:56 +0700

    add model

--- React/graphql-playlist/server/schema/schema.js
+++ React/graphql-playlist/server/schema/schema.js
@@ -1,5 +1,8 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../model/book');
+const Author = require('../model/author');
+
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -9,55 +12,51 @@ const {
     GraphQLList
 } = graphql;
 
-var books = [
-    {
-        name: 'Name of the Wind',
-        genre: 'Fantasy',
-        id: '1',
-        authorId: '1'
-    }, {
-        name: 'The Final Empire',
-        genre: 'Fantasy',
-        id: '2',
-        authorId: '2'
-    }, {
-        name: 'The Hero of Ages',
-        genre: 'Fantasy',
-        id: '4',
-        authorId: '2'
-    }, {
-        name: 'The Long Earth',
-        genre: 'Sci-Fi',
-        id: '3',
-        authorId: '3'
-    }, {
-        name: 'The Colour of Magic',
-        genre: 'Fantasy',
-        id: '5',
-        authorId: '3'
-    }, {
-        name: 'The Light Fantastic',
-        genre: 'Fantasy',
-        id: '6',
-        authorId: '3'
-    }
-];
+var books = [{
+    name: 'Name of the Wind',
+    genre: 'Fantasy',
+    id: '1',
+    authorId: '1'
+}, {
+    name: 'The Final Empire',
+    genre: 'Fantasy',
+    id: '2',
+    authorId: '2'
+}, {
+    name: 'The Hero of Ages',
+    genre: 'Fantasy',
+    id: '4',
+    authorId: '2'
+}, {
+    name: 'The Long Earth',
+    genre: 'Sci-Fi',
+    id: '3',
+    authorId: '3'
+}, {
+    name: 'The Colour of Magic',
+    genre: 'Fantasy',
+    id: '5',
+    authorId: '3'
+}, {
+    name: 'The Light Fantastic',
+    genre: 'Fantasy',
+    id: '6',
+    authorId: '3'
+}];
 
-var authors = [
-    {
-        name: 'Patrick Rothfuss',
-        age: 44,
-        id: '1'
-    }, {
-        name: 'Brandon Sanderson',
-        age: 42,
-        id: '2'
-    }, {
-        name: 'Terry Pratchett',
-        age: 66,
-        id: '3'
-    }
-];
+var authors = [{
+    name: 'Patrick Rothfuss',
+    age: 44,
+    id: '1'
+}, {
+    name: 'Brandon Sanderson',
+    age: 42,
+    id: '2'
+}, {
+    name: 'Terry Pratchett',
+    age: 66,
+    id: '3'
+}];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -75,7 +74,7 @@ const BookType = new GraphQLObjectType({
             type: AuthorType,
             resolve(parent, args) {
                 //code to get data from db
-                return _.find(authors, {id: parent.authorId})
+                //return _.find(authors, {id: parent.authorId})
             }
         }
     })
@@ -96,7 +95,7 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return _.filter(books, {authorId: parent.id})
+                //return _.filter(books, {authorId: parent.id})
             }
         }
     })
@@ -114,7 +113,7 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 //code to get data from db
-                return _.find(books, {id: args.id})
+                //return _.find(books, {id: args.id})
             }
         },
         author: {
@@ -126,22 +125,24 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 //code to get data from db
-                return _.find(authors, {id: args.id})
+                //return _.find(authors, {id: args.id})
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books;
+                //return books;
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                return authors;
+                //return authors;
             }
         }
     }
 })
 
-module.exports = new GraphQLSchema({query: RootQuery})
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
