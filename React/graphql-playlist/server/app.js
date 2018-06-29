047195bea07450da45da34a49c93fb48042af5f4
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-29 12:54:05 +0700

    add mongoose

--- React/graphql-playlist/server/app.js
+++ React/graphql-playlist/server/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const grapqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect("mongodb://phucngo:phucngo95@ds121311.mlab.com:21311/gpl");
+mongoose.connection.once('open', () => {
+    console.log('connection has been made');
+});
 
 app.use('/graphql', grapqlHTTP({
     schema,
