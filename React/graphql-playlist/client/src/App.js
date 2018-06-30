69faa83ec1b1bf2bae23e83bccd77b7277dc8d0f
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-30 09:35:10 +0700

    add ApolloClient

--- React/graphql-playlist/client/src/App.js
+++ React/graphql-playlist/client/src/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import BookList from './components/BookList';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
+//apolloCLient setup
+const client = new ApolloClient({url: 'http://localhost:4000/graphql'});
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
         <div id="main">
           <h1>Ninja's Reading List</h1>
-          <BookList />
-      </div>
+          <BookList/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
