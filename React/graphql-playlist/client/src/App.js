b8478f52c12d91ebdb9368053fb5b9f07d1f2660
Tony Hudson <ngohungphuc95@gmail.com>
2018-06-30 10:05:42 +0700

    get authors

--- React/graphql-playlist/client/src/App.js
+++ React/graphql-playlist/client/src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 import './App.css';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
 
 //apolloCLient setup
 const client = new ApolloClient({uri: 'http://localhost:4000/graphql'});
@@ -14,6 +15,7 @@ class App extends Component {
         <div id="main">
           <h1>Ninja's Reading List</h1>
           <BookList/>
+          <AddBook/>
         </div>
       </ApolloProvider>
     );
