1d8a7d1be2430a3c4a59515a828759708fb2e8ef
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-01 20:49:25 +0700

    add options for popup

--- Electron/boilerplates/tasky/index.js
+++ Electron/boilerplates/tasky/index.js
@@ -8,6 +8,11 @@ const {
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false
+    });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
