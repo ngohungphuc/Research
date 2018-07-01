368f3a765801efa61e8fb70f4d49ee20371a5813
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-01 21:36:04 +0700

    toggle show and hide

--- Electron/boilerplates/tasky/index.js
+++ Electron/boilerplates/tasky/index.js
@@ -7,17 +7,25 @@ const {
 } = electron;
 
 let mainWindow;
-
+let tray;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         height: 500,
         width: 300,
         frame: false,
         resizable: false,
+        show: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-    new Tray(iconPath);
+    tray = new Tray(iconPath);
+    tray.on('click', () => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            mainWindow.show();
+        }
+    });
 });
