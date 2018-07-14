965997dc65d0dff9ad33541dc1aba5b5d4cbabaf
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-14 21:34:06 +0700

    strategy network with cache fallback

--- Progressive Web App/course-project/public/sw.js
+++ Progressive Web App/course-project/public/sw.js
@@ -74,3 +74,19 @@ self.addEventListener('activate', function (event) {
 /* self.addEventListener("fetch", function (event) {
   event.respondWith(fetch(event.request));
 }); */
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(res) {
+        return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+      })
+      .catch(function(err) {
+        return caches.match(event.request);
+      })
+  );
+});
