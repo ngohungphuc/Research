63ebdeeef7f66c745daf10812fb3df2fd5438d47
Tony Hudson <ngohungphuc95@gmail.com>
2018-08-23 14:41:53 +0700

    add pic

---
+++ ASP.Net/Masstransit/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿using MassTransit;
+using System;
+
+namespace ConsoleApp6
+{
+    public class YourMessage { public string Text { get; set; } }
+    public class Program
+    {
+        public static void Main()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                sbc.ReceiveEndpoint(host, "test_queue", ep =>
+                {
+                    ep.Handler<YourMessage>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            bus.Start();
+
+            bus.Publish(new YourMessage { Text = "Hi" });
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
