5ddf014af634c098b19de5cf373fd936e5e2ff0f
ngohungphuc <ngohungphuc95@gmail.com>
2018-10-11 15:40:44 +0700

    update sample

--- Progressive Web App/progressive-web-apps-book/chapter-3/cachefirst/sw-cachefirst.js
+++ Progressive Web App/progressive-web-apps-book/chapter-3/cachefirst/sw-cachefirst.js
@@ -1,27 +1,30 @@
 var cacheName = 'helloWorld';
-
-self.addEventListener('fetch', function(event) {
+//Add an event listener for the fetch event to intercept requests.
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      if (response) {
+    caches.match(event.request) // Does the current request match anything you might have in cache?
+    .then(function (response) {
+      if (response) { // If it does, return it at this point and continue no further.
         return response;
       }
 
-      var fetchRequest = event.request.clone();
+      var fetchRequest = event.request.clone(); // Clone the request— a request is a stream and can only be consumed once.
 
+      //try to make original HTTP request as intended
       return fetch(fetchRequest).then(
-        function(fetchResponse) {
-          if(!fetchResponse || fetchResponse.status !== 200) {
+        function (fetchResponse) {
+          if (!fetchResponse || fetchResponse.status !== 200) {
             return fetchResponse;
           }
 
+          // again clone the res because you need to add it into cache and because it's used for final response
           var responseToCache = fetchResponse.clone();
 
+          //open and add response into cache
           caches.open(cacheName)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
+            .then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
 
           return fetchResponse;
         }
