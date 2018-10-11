27c97dc5104b46acce0ef1fb4fbbac7f5807647c
ngohungphuc <ngohungphuc95@gmail.com>
2018-10-11 15:27:26 +0700

    doc example

--- Progressive Web App/progressive-web-apps-book/chapter-3/precache/service-worker.js
+++ Progressive Web App/progressive-web-apps-book/chapter-3/precache/service-worker.js
@@ -1,23 +1,23 @@
-var cacheName = 'helloWorld';
+var cacheName = 'helloWorld'; //Name of the cache
 
-self.addEventListener('install', event => {
+self.addEventListener('install', event => { //Tap into the Service Worker install event
   event.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll([
+    caches.open(cacheName) // Open a cache using the cache name we specified
+    .then(cache => cache.addAll([ //Add the JavaScript and image into the cache
       './js/script.js',
       './images/hello.png'
     ]))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
+    caches.match(event.request) // Check whether incoming request URL matches anything that exists in the current cache
+    .then(function (response) {
       if (response) {
-        return response;
+        return response; // If there’s a response and it isn’t undefined/null, then return it
       }
-      return fetch(event.request);
+      return fetch(event.request); //  Else continue as normal and fetch the resource as intended
     })
   );
 });
