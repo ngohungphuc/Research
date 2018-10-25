029a75b0dd7110d434f070e51c36ff0ae14836d0
ngohungphuc <ngohungphuc95@gmail.com>
2018-10-25 13:27:14 +0700

    add mongodb

--- GraphQL/graphql-project/index.js
+++ GraphQL/graphql-project/index.js
@@ -3,6 +3,17 @@ const {
 } = require('graphql');
 const readline = require('readline');
 const mySchema = require('./schema/main');
+const {
+    MongoClient
+} = require('mongodb');
+const assert = require('assert');
+const MONGO_URL = 'mongodb://localhost:27017/graphql';
+
+MongoClient.connect(MONGO_URL, (err, db) => {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB server');
+    // The readline interface code
+});
 
 const rli = readline.createInterface({
     input: process.stdin,
