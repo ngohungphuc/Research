476a1521354db1a894c87beff78ca66ab090007d
Tony Hudson <ngohungphuc95@gmail.com>
2018-11-18 23:01:11 +0700

    branching and looping

--- GoLang/GoGettingStarted/src/main.go
+++ GoLang/GoGettingStarted/src/main.go
@@ -5,18 +5,37 @@ import (
 )
 
 func main() {
-	var planCapacitites []float64
+	planCapacitites := []float64{30, 30, 30, 60, 60, 100}
 
-	planCapacitites = []float64{30, 30, 30, 60, 60, 100}
+	activePlants := []int{0, 1}
 
-	var capacity float64 = planCapacitites[0] + planCapacitites[1] +
-		planCapacitites[2] + planCapacitites[3] + planCapacitites[4] + planCapacitites[5]
+	gridLoad := 75.
 
-	var gridLoad = 75.
+	fmt.Println("1) Generate Power Plan Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Print("Please chose an option: ")
 
-	utilization := gridLoad / capacity
+	var option string
 
-	fmt.Printf("%-20s%.0f\n", "Capacity", capacity)
-	fmt.Printf("%-20s%.0f\n", "Load", gridLoad)
-	fmt.Printf("%-20s%.1f%%\n", "Util", utilization*100)
+	//& pass memory address of variable to Scanln func
+	fmt.Scanln(&option)
+
+	switch option {
+	case "1":
+		for idx, cap := range planCapacitites {
+			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+		}
+
+	case "2":
+		capacity := 0.
+		for _, plantId := range activePlants {
+			capacity += planCapacitites[plantId]
+		}
+		fmt.Printf("%-20s%.0f\n", "Capacity", capacity)
+		fmt.Printf("%-20s%.0f\n", "Load", gridLoad)
+		fmt.Printf("%-20s%.1f%%\n", "Util", gridLoad/capacity*100)
+
+	default:
+		fmt.Println("Unknow command")
+	}
 }
