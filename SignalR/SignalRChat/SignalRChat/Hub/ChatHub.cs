4d42d179973ebd4b1fa28a047046c576cc95e2f7
ngohungphuc <ngohungphuc95@gmail.com>
2018-12-04 16:55:40 +0700

    add sample signalr

---
+++ SignalR/SignalRChat/SignalRChat/Hub/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hub
+{
+    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
