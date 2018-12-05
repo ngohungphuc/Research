6913125c3c815698e8dedc74d473db9861346dfa
Tony Hudson <ngohungphuc95@gmail.com>
2018-12-05 23:14:36 +0700

    add model

---
+++ ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.API.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
