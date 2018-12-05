5bd114e4f8509e176493e28a3f000fe43cda9aa7
Tony Hudson <ngohungphuc95@gmail.com>
2018-12-06 00:25:33 +0700

    update controller

--- ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Controllers/CustomersController.cs
+++ ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Controllers/CustomersController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BankOfDotNet.API.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BankOfDotNet.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CustomersController : ControllerBase
