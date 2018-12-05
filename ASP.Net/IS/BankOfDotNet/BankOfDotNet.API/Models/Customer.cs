dac8fffe38eeeab33036cfe248538efccd8b5734
Tony Hudson <ngohungphuc95@gmail.com>
2018-12-05 23:20:59 +0700

    add db config

--- ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Models/Customer.cs
+++ ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Models/Customer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BankOfDotNet.API.Models
+﻿namespace BankOfDotNet.API.Models
 {
     public class Customer
     {
--- ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Startup.cs
+++ ASP.Net/IS/BankOfDotNet/BankOfDotNet.API/Startup.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace BankOfDotNet.API
 {
@@ -24,6 +19,7 @@ namespace BankOfDotNet.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbContext>(opts => opts.UseInMemoryDatabase("BankingDB"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
