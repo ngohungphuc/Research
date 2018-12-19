920d47289bd3464a8416ead6f2ca9b32a528cc7c
ngohungphuc <ngohungphuc95@gmail.com>
2018-12-19 17:37:20 +0700

    update code

---
+++ ASP.Net/ScheduledService/SS/ScheduledService/Services/IHostedService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScheduledService.Services
+{
+    public interface IHostedService
+    {
+        //
+        // Summary:
+        //     Triggered when the application host is ready to start the service.
+        Task StartAsync(CancellationToken cancellationToken);
+        //
+        // Summary:
+        //     Triggered when the application host is performing a graceful shutdown.
+        Task StopAsync(CancellationToken cancellationToken);
+    }
+}
