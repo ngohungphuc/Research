6ee568ccf3f809666e831870737477a02f4ae932
Tony Hudson <ngohungphuc95@gmail.com>
2019-02-07 11:32:48 +0700

    add demo jquery

---
+++ Angular/type-extensions/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'type-extensions';
+  ngOnInit(): void {
+    $("#test").text("hello");
+  }
+}
