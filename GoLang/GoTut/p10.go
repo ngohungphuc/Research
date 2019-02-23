81dbce58d4e3e645a7bcfbbe8a7a05e385520552
Tony Hudson <ngohungphuc95@gmail.com>
2019-02-23 22:39:13 +0700

    parse xml

--- GoLang/GoTut/p10.go
+++ GoLang/GoTut/p10.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type SiteMapIndex struct {
+	Locations []Location `xml:"url"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
 func main() {
 	resp, _ := http.Get("https://www.xml-sitemaps.com/download/ngohungphuc.wordpress.com-747582882/sitemap.xml?view=1")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
 	resp.Body.Close()
+
+	var s SiteMapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 }
