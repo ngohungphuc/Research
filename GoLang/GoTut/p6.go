97a2660a144fdd45258f1e343e01618da240e78e
Tony Hudson <ngohungphuc95@gmail.com>
2019-02-23 21:34:48 +0700

    u

--- GoLang/GoTut/p6.go
+++ GoLang/GoTut/p6.go
@@ -4,17 +4,23 @@ import (
 	"fmt"
 )
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
-	gas_pedal uint16
-	brake_pedal uint16
+	gas_pedal      uint16
+	brake_pedal    uint16
 	steering_wheel int16
-	top_speed_kmh float64
+	top_speed_kmh  float64
+}
+
+func (c car) kmh() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
 }
 
 func main() {
-	a_car := car {
-		gas_pedal: 22341, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0
-	}
+	a_car := car{gas_pedal: 22341, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0}
 
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
 }
