7e30dcccb0cabdfbafcedd881194c636924ffb40
Tony Hudson <ngohungphuc95@gmail.com>
2019-02-23 22:07:33 +0700

    u

--- GoLang/GoTut/p6.go
+++ GoLang/GoTut/p6.go
@@ -19,9 +19,17 @@ func (c car) kmh() float64 {
 	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
 }
 
+//pointer receiver
+func (c *car) new_top_speed(newspeed float64) {
+	c.top_speed_kmh = newspeed
+}
+
 func main() {
 	a_car := car{gas_pedal: 22341, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0}
 
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+
+	a_car.new_top_speed(500)
 	fmt.Println(a_car.kmh())
 }
