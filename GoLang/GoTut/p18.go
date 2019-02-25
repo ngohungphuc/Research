df0adf5d931f53d2ff9ab13d2ac5f44ed20f2ea3
ngohungphuc <ngohungphuc95@gmail.com>
2019-02-25 15:39:24 +0700

    Goroutine Synchronization

--- GoLang/GoTut/p18.go
+++ GoLang/GoTut/p18.go
@@ -2,16 +2,23 @@ package main
 import (
 	"time"
 	"fmt"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func say(s string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond*100)
 	}
+	wg.Done()
 }
 
 func main() {
+	wg.Add(1)
 	go say("Hey")
-	say("There")
+	wg.Add(1)
+	go say("There")
+	wg.Wait()
 }
