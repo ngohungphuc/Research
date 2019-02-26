0133e34c19bd39498ff39ea4aa0afa11fcebd1a7
Tony Hudson <ngohungphuc95@gmail.com>
2019-02-26 23:31:06 +0700

    u

---
+++ GoLang/GoWebProgramming/Chap3/3.12.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
+}
+func main() {
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
