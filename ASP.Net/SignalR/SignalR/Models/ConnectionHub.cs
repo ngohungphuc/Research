23395fc87d42873a86f74424c191ee7718152403
Tony Hudson <ngohungphuc95@gmail.com>
2019-05-01 20:20:00 +0700

    Add signalr sample

---
+++ ASP.Net/SignalR/SignalR/Models/ConnectionHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Models
+{
+    public class ConnectionHub : Hub
+    {
+        public async Task Send(string userId)
+        {
+            var message = $"Send message to you with user id {userId}";
+            await Clients.Client(userId).SendAsync("ReceiveMessage", message);
+        }
+
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+        public void SendMessageToSpecificUser()
+        {
+
+        }
+    }
+}
