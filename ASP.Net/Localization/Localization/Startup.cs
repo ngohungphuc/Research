b8039dec88f617b296ae6ccc723ce7ddcb1f087a
ngohungphuc <ngohungphuc95@gmail.com>
2019-05-24 22:51:10 +0700

    update

--- ASP.Net/Localization/Localization/Startup.cs
+++ ASP.Net/Localization/Localization/Startup.cs
@@ -28,41 +28,14 @@ namespace Localization
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddLocalization(options => options.ResourcesPath = "Resources");
-
-            services.AddMvc()
-                .AddViewLocalization(
-                    opts => { opts.ResourcesPath = "Resources"; })
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddViewLocalization(options => options.ResourcesPath = "Resources")
                 .AddDataAnnotationsLocalization(options =>
                 {
                     options.DataAnnotationLocalizerProvider = (type, localizerFactory) =>
                         localizerFactory.Create(typeof(Resource));
                 });
-
-            services.Configure<RequestLocalizationOptions>(
-                    opts =>
-                    {
-                        var supportedCultures = new List<CultureInfo>
-                        {
-                                new CultureInfo("en"),
-                                new CultureInfo("nl")
-                        };
-
-                        opts.DefaultRequestCulture = new RequestCulture("en");
-                        // Formatting numbers, dates, etc.
-                        opts.SupportedCultures = supportedCultures;
-                        // UI strings that we have localized.
-                        opts.SupportedUICultures = supportedCultures;
-                    });
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
