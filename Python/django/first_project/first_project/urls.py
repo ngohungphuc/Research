38a46dbe72f359d7af0db1d7e75b1294cfc947d2
ngohungphuc <ngohungphuc95@gmail.com>
2019-08-31 23:13:35 +0700

    render hello world text

--- Python/django/first_project/first_project/urls.py
+++ Python/django/first_project/first_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
 
 urlpatterns = [
+    path('',views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
