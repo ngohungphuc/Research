e4b0a78376224fca37c1da161cb507737ca23370
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-01 10:54:27 +0700

    render data to html view

--- Python/django/first_project/first_app/views.py
+++ Python/django/first_project/first_app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello world")
+    my_dict = {'insert_me': 'hello from dir'}
+    return render(request, 'first_app/index.html', context=my_dict)
