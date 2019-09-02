9b1450b48ba70c887c7ba5ae349f9ed45f5776c7
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-02 19:55:37 +0700

    display data come from models

--- Python/django/first_project/first_app/views.py
+++ Python/django/first_project/first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord, Topic, WebPage
 # Create your views here.
 
 
 def index(request):
-    my_dict = {'insert_me': 'hello from dir'}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
