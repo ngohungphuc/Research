bfd51feff164baaf60adb5ceda611dc8870d0304
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-03 14:19:39 +0700

    add validation

--- Python/django/first_project/first_app/forms.py
+++ Python/django/first_project/first_app/forms.py
@@ -5,3 +5,10 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError("opps")
+        return botcatcher
