4dce38a19dae80d1bf736a655cfe0daf7cc1682d
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-03 14:35:52 +0700

    update code

--- Python/django/first_project/first_app/forms.py
+++ Python/django/first_project/first_app/forms.py
@@ -1,14 +1,26 @@
 from django import forms
+from django.core import validators
 
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+    verify_email = forms.EmailField(label='enter email again')
+    botcatcher = forms.CharField(
+        required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
 
-    def clean_botcatcher(self):
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+        if email != vmail:
+            raise forms.ValidationError('Make email and vemail same')
+
+
+"""     def clean_botcatcher(self):
         botcatcher = self.cleaned_data['botcatcher']
         if len(botcatcher) > 0:
             raise forms.ValidationError("opps")
         return botcatcher
+ """
