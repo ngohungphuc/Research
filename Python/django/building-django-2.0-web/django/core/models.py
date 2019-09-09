90f4eaf28bc28b4428a0fdea3222b082be471d3a
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-09 14:54:10 +0700

    add model

--- Python/django/building-django-2.0-web/django/core/models.py
+++ Python/django/building-django-2.0-web/django/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    NOT_RATED = 0
+    RATED_G = 1
+    RATED_PG = 2
+    RATED_R = 3
+    RATING = (
+        (NOT_RATED, 'NR - Not Rated'),
+        (RATED_G, 'G - General Audiences'),
+        (RATED_PG, 'PG - Parental Guidance' 'Suggested'),
+        (RATED_R, 'R - Restricted')
+    )
+
+    title = models.CharField(max_length=140)
+    plot = models.TextField()
+    year = models.PositiveIntegerField()
+    rating = models.IntegerField(choices=RATING, default=NOT_RATED)
+    runtime = models.PositiveIntegerField()
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return '{} ({})'.format(self.title, self.year)
