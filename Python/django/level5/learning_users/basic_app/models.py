bb0fcfee9160e1bbad366a67cccfd9b82f132fb0
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-14 19:14:26 +0700

    define model

--- Python/django/level5/learning_users/basic_app/models.py
+++ Python/django/level5/learning_users/basic_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+
+    portfolio_site = models.URLField(blank=True)
+    # upload to profile_pics folder
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
