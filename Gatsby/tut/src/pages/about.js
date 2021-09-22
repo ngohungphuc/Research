6ad3b76b6e1cf64f9b8a94cb9f4b74273b41a1cb
ngohungphuc <ngohungphuc95@gmail.com>
2021-09-22 21:47:57 +0700

    add

---
+++ Gatsby/tut/src/pages/about.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class contact extends Component {
+    render() {
+        return (
+            <div>
+                Contact
+            </div>
+        )
+    }
+}
